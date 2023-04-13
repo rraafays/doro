371ef2108c21fcfe62a52426b7bbd7033b61841b
raf <rraf@tuta.io>
2023-04-13 17:31:56 +0100

    update: started writing functions for each operation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#![allow(dead_code)]
+
 fn main() {
-  println!("i will be a terminal pomodoro timer written in rust");
+
+}
+
+enum Operation {
+  Reset,
+  Break,
+  None
+}
+
+struct Time {
+  minutes: u8,
+  seconds: u8
+}
+
+fn timer (time: Time, message: String) {
+  
+}
+
+fn get_operation(minutes: u8, seconds: u8) -> Operation {
+  if seconds == 0 {  return Operation::Reset }
+  if seconds == 0 && minutes == 0 { return Operation::Break }
+  else { return Operation::None }
+}
+
+fn reset_seconds(mut time: Time) -> Time {
+  time.seconds = 60;
+  time.minutes -= 1;
+  return time
 }
