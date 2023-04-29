a289a41ee467c5f7afcf0c60a35bb9692790294c
raf <rraf@tuta.io>
2023-04-29 14:51:22 +0100

    update: broke down into module files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+mod utils;
+
 use std::thread::sleep;
 use std::time::Duration;
-use std::io::Write;
 use clap::Parser;
-use colored::Colorize;
+use utils::{ 
+  Time, 
+  Operation, 
+  Mode, 
+  print_time, 
+  get_operation, 
+  reset_seconds, 
+  advance 
+};
 
 #[derive(Parser)]
 struct Arguments {
@@ -22,22 +31,6 @@ fn main() {
   }
 }
 
-enum Operation {
-  Reset,
-  Break,
-  None
-}
-
-enum Mode {
-  Work,
-  Rest
-}
-
-struct Time {
-  minutes: u8,
-  seconds: u8
-}
-
 fn timer (time: Time, mode: Mode) {
   let mut remaining_time: Time = time;
   loop {
@@ -46,28 +39,7 @@ fn timer (time: Time, mode: Mode) {
     match get_operation(&remaining_time) {
       Operation::Break => break,
       Operation::Reset => reset_seconds(&mut remaining_time),
-      Operation::None => remaining_time.seconds -= 1
+      Operation::None => advance(&mut remaining_time)
     }
   }
-}
-
-fn get_operation(time: &Time) -> Operation {
-  if time.seconds == 0 && time.minutes == 0 { return Operation::Break }
-  if time.seconds == 0 {  return Operation::Reset }
-  else { return Operation::None }
-}
-
-fn reset_seconds(mut time: &mut Time) {
-  time.seconds = 59;
-  if time.minutes > 0 { time.minutes -= 1; }
-}
-
-fn print_time(time: &Time, mode: &Mode) {
-  let symbol;
-  match mode {
-    Mode::Work => symbol = "WORK ".bright_red().bold(),
-    Mode::Rest => symbol = "REST ".bright_blue().bold()
-  }
-  print!("\r{}{:0>2}:{:0>2}", symbol, time.minutes, time.seconds);
-  std::io::stdout().flush().unwrap();
 }
