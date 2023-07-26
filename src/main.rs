b9aa3322bcaf196746a12c35384f079477bba711
raf <rraf@tuta.io>
2023-07-26 11:27:32 +0100

    chore: reformatting

--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,43 @@ use utils::{advance, get_operation, print_time, reset_seconds, Mode, Operation, 
 
 #[derive(Parser)]
 struct Arguments {
-    #[arg(short, long, default_value_t = 25)]
-    work: u8,
-    #[arg(short, long, default_value_t = 5)]
-    rest: u8,
+  #[arg(short, long, default_value_t = 25)]
+  work: u8,
+  #[arg(short, long, default_value_t = 5)]
+  rest: u8,
 }
 
 fn main() {
-    let arguments = Arguments::parse();
-    loop {
-        timer(
-            Time {
-                minutes: arguments.work,
-                seconds: 0,
-            },
-            Mode::Work,
-        );
-        print!("\x07");
-        timer(
-            Time {
-                minutes: arguments.rest,
-                seconds: 0,
-            },
-            Mode::Rest,
-        );
-        print!("\x07");
-    }
+  let arguments = Arguments::parse();
+  loop {
+    timer(
+      Time {
+        minutes: arguments.work,
+        seconds: 0,
+      },
+      Mode::Work,
+    );
+    print!("\x07");
+    timer(
+      Time {
+        minutes: arguments.rest,
+        seconds: 0,
+      },
+      Mode::Rest,
+    );
+    print!("\x07");
+  }
 }
 
 fn timer(time: Time, mode: Mode) {
-    let mut remaining_time: Time = time;
-    loop {
-        print_time(&remaining_time, &mode);
-        sleep(Duration::from_secs(1));
-        match get_operation(&remaining_time) {
-            Operation::Break => break,
-            Operation::Reset => reset_seconds(&mut remaining_time),
-            Operation::None => advance(&mut remaining_time),
-        }
+  let mut remaining_time: Time = time;
+  loop {
+    print_time(&remaining_time, &mode);
+    sleep(Duration::from_secs(1));
+    match get_operation(&remaining_time) {
+      Operation::Break => break,
+      Operation::Reset => reset_seconds(&mut remaining_time),
+      Operation::None => advance(&mut remaining_time),
     }
+  }
 }
